549b205befb753b933aac26e5a90acdda8ffda2b
av.sheremetev <av.sheremetev@tensor.ru>
2019-04-24 13:59:23 +0300

    Hello world

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 app.engine('html', require('ejs').renderFile);
@@ -6,3 +7,12 @@ app.get('/', function(req, res) {
 });
 
 app.listen(8080);
+*/
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello Http');
+});
+
+server.listen(8080);
