189453430e46c1ac6de184987e0b890d7578a393
av.sheremetev <av.sheremetev@tensor.ru>
2019-04-24 13:51:37 +0300

    Установил express

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-var
-    express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 app.engine('html', require('ejs').renderFile);
 app.get('/', function(req, res) {
     res.sendfile('index.html');
